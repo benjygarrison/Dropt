d54d2d35e5a6ad4faffda8df3d8ce48b9f3ae1e6
benjyGarrison <benjygarrison@gmail.com>
2019-08-31 20:23:07 -0700

    Buttons work; button press displays in text field

--- PASSWORD/ViewController.swift
+++ PASSWORD/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var passCodeText: UITextField!
+    //var passCodeDefault = ""
+    //passCodeText.text = passCodeDefault
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,8 +21,35 @@ class ViewController: UIViewController {
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         
-        print("Hi")
-        
+        switch sender.tag {
+        case 0:
+            passCodeText.text = "1"
+        case 1:
+            passCodeText.text = "2"
+        case 2:
+            passCodeText.text = "3"
+        case 3:
+            passCodeText.text = "4"
+        case 4:
+            passCodeText.text = "5"
+        case 5:
+            passCodeText.text = "6"
+        case 6:
+            passCodeText.text = "7"
+        case 7:
+            passCodeText.text = "8"
+        case 8:
+            passCodeText.text = "9"
+        case 9:
+            passCodeText.text = "0"
+        case 10:
+            print("back")
+        case 11:
+            passCodeText.text = ""
+        default:
+            print("hubba hubba")
+            
+        }
     }
     
 }
