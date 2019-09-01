6258f8b0be9169a3ca9b60a6049db68674733a43
benjyGarrison <benjygarrison@gmail.com>
2019-08-31 20:38:18 -0700

    Buttons work, append to text field

--- PASSWORD/ViewController.swift
+++ PASSWORD/ViewController.swift
@@ -11,37 +11,39 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var passCodeText: UITextField!
-    //var passCodeDefault = ""
-    //passCodeText.text = passCodeDefault
+    var passCodeDefault: String = ""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        passCodeText.text = passCodeDefault
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         
         switch sender.tag {
         case 0:
-            passCodeText.text = "1"
+            passCodeText.text? += "1"
         case 1:
-            passCodeText.text = "2"
+            passCodeText.text? += "2"
         case 2:
-            passCodeText.text = "3"
+            passCodeText.text? += "3"
         case 3:
-            passCodeText.text = "4"
+            passCodeText.text? += "4"
         case 4:
-            passCodeText.text = "5"
+            passCodeText.text? += "5"
         case 5:
-            passCodeText.text = "6"
+            passCodeText.text? += "6"
         case 6:
-            passCodeText.text = "7"
+            passCodeText.text? += "7"
         case 7:
-            passCodeText.text = "8"
+            passCodeText.text? += "8"
         case 8:
-            passCodeText.text = "9"
+            passCodeText.text? += "9"
         case 9:
-            passCodeText.text = "0"
+            passCodeText.text? += "0"
         case 10:
             print("back")
         case 11:
